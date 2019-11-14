567fde87376c4b954a76c4ec933924cecbab9e66
han-ubuntu <xuyuan.HAN@outlook.com>
2019-11-14 20:30:55 +0800

    finish adjust EXPOSURE

--- main.cpp
+++ main.cpp
@@ -91,10 +91,10 @@ int main()
 
 
     #ifdef USE_VIDEO //使用录像
-    // capture.open("../Video/2019-10-28-222635.webm");
-    // capture.open("../Video/2019-10-28-223802.webm");
-    capture.open("../Video/2019-10-28-223826.webm");
-    // capture.open("../Video/2019-10-28-223848.webm");
+    // capture.open("../Video/Webcam/2019-10-28-222635.webm");
+    // capture.open("../Video/Webcam/2019-10-28-223802.webm");
+    capture.open("../Video/Webcam/2019-10-28-223826.webm");
+    // capture.open("../Video/Webcam/2019-10-28-223848.webm");
     cv::Mat src;
     cv::Mat src_real;
 
